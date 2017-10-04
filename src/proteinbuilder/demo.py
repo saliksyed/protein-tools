c4417986420aeaf5b71d7ded3130cd117d970b3c
Salik Syed <ssyed@cs.stanford.edu>
2017-10-03 23:31:59 -0700

    add energy calculation (unoptimized)

--- src/proteinbuilder/demo.py
+++ src/proteinbuilder/demo.py
@@ -11,22 +11,16 @@ f = ForceField()
 villinN68H = """LSDEDFKAVFGMTRSAFANLPLWKQQHLKKEKGLF"""
 c = f.create_chain(villinN68H)
 c.write_to_pdb("data/output.pdb")
+
 # Sample Conformations:
-count = 0
-start = time.time()
 def sample_conformation():
 	global count, start
 	conformation = c.get_conformation()
 	while True:
-		try:
-			conformation[random.randint(0, len(conformation)-1)][1] += 0.5
-			c.set_conformation(conformation)
-			count += 1
-			if count%10 == 0:
-			    print "%d samples evaluated in %.2f seconds" % (count, (time.time() - start))
-			time.sleep(0.1)
-		except:
-			traceback.print_exc()
+		conformation[random.randint(0, len(conformation)-1)][1] += 0.5
+		c.set_conformation(conformation)
+		print f.get_energy(c)
+		time.sleep(0.01)
 
 t = threading.Thread(target=sample_conformation)
 t.start()
